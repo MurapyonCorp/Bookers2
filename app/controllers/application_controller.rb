3541f9049d0546e0724192a927c21b9325386dbb
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-05-13 03:03:48 +0000

    課題休憩

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
