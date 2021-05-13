470b253e7b790dfb874f3ae5c04729430eb33b05
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-05-13 13:57:01 +0000

    課題中断

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :users
   root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  get "home/about" => "homes#about"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
