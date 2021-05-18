a9ad66582dd6df39de901290fa20dcfdd07ed70f
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-05-18 14:16:47 +0000

    Good night

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,9 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
   end
 
   def update
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,9 @@ class UsersController < ApplicationController
   end
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
