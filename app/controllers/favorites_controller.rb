eff119fcaf00c3d72c75f81b73e7e68d3d3708d2
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-06-04 09:50:33 +0000

    非同期通信terminal no reaction

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,11 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to request.referer
   end
 end
