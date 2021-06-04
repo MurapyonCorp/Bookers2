c9a62892d593d4561048228b7a33f709835b3f8d
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-06-04 14:41:02 +0000

    js.erb書いた remoteまだ

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,12 +6,12 @@ class BookCommentsController < ApplicationController
     comment.save
     redirect_to book_path(book)
   end
-  
+
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     redirect_to book_path(params[:book_id])
   end
-  
+
   private
   def book_comment_params
     params.require(:book_comment).permit(:comment)
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,15 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
   end
 end
