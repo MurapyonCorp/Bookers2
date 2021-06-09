5b105e7de43f8cdac4d01a2af551d3b3454b04ae
Ryoooo0811 <s21ah094@seinan-gakuin.jp>
2021-06-09 07:04:27 +0000

    follow-follower complete！

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get "home/about" => "homes#about"
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    # ——————————————— ここから ———————————————
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+    # ——————————— ここまでネストさせる ———————————
+  end
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
